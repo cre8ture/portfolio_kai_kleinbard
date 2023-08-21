0d3ce6979373782627e0cfa61ec0648c26c70c3b
Kaik <bakukai@gmail.com>
2023-08-20 20:59:22 -0400

    new

--- src/components/google/GoogleAnalytics.js
+++ src/components/google/GoogleAnalytics.js
@@ -5,10 +5,13 @@ const GoogleAnalytics = ({ GA_TRACKING_ID }) => {
   return (
     <>
       <Script
+       id="google-analytics1"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
         strategy="afterInteractive"
       />
-      <Script strategy="afterInteractive">
+      <Script 
+      id="google-analytics2"
+       strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
